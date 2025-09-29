529b128f04670aa773fa79979cc160026ca3214e
unknown <alina.yaroslavovna@gmail.com>
2025-09-29 16:19:15 +0300

    initial comit

---
+++ src/test/java/ru/netology/services/PostmanEchoTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    private static final String TEST_BODY_CONTENT = "Hello from Java Test!";
+
+    @BeforeAll
+    public static void setup() {
+        // Установка базового URI для всех тестов
+        RestAssured.baseURI = "https://postman-echo.com";
+    }
+
+    @Test
+    @DisplayName("Проверка успешной отправки простого тела и его возврата в поле 'data'")
+    public void testPostRequestReturnsSentDataInBody() {
+        // Given: Подготовка
+        given()
+                .body(TEST_BODY_CONTENT)
+                // When: Выполнение POST-запроса к /post
+                .when()
+                .post("/post")
+                // Then: Проверки
+                .then()
+                .statusCode(200)
+                // JSONPath-выражение: так как ответ { "data": "..." }, мы обращаемся напрямую к полю 'data'
+                .body("data", equalTo(TEST_BODY_CONTENT));
+    }
+}
